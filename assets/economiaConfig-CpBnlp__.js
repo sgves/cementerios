449e9d67a79d54ba3161d828370faed66dd9d5b7
amurillo <antonio@sgv.es>
2026-06-17 08:11:09 +0200

    Updates

---
+++ assets/economiaConfig-CpBnlp__.js
@@ -0,0 +1,1 @@
+import{zt as e}from"./index-DSmh5wPM.js";async function t(t){return e.from(`cementerio_config_economica`).select(`codigo_cementerio,opciones`).eq(`codigo_cementerio`,t).maybeSingle()}export{t};
